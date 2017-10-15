0b1685ce7311c92d731ff3fef61ad0bb1e8df1e3
Kunal Dabir <kunal.dabir@gmail.com>
2017-10-15 11:44:17 +0530

    create basic layout

--- src/App.js
+++ src/App.js
@@ -1,21 +1,55 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
+import AceEditor from "react-ace";
+import "brace/mode/json";
+import "brace/mode/sql";
+import "brace/theme/solarized_dark";
+import "brace/ext/language_tools";
+
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+    runSql(json, sql) {
+        console.log('running SQL', json, sql);
+    }
+
+    render() {
+        return (
+            <div className="App">
+
+                <div className="row">
+                    <div className='column'>
+                        <p>Json Data</p>
+                        <AceEditor
+                            style={{width: '100%'}}
+                            name="jsonEditor"
+                            mode="json"
+                            theme="solarized_dark"
+                            ref='jsonEditor'/>
+                    </div>
+                    <div className='column'>
+                        <p>SQL</p>
+                        <AceEditor
+                            style={{width: '100%'}}
+                            name='sqlEditor'
+                            mode="sql"
+                            theme="solarized_dark"
+                            ref='sqlEditor'/>
+                    </div>
+                </div>
+
+                <button onClick={() => {
+                    this.runSql(
+                        this.refs.jsonEditor.editor.getValue(),
+                        this.refs.sqlEditor.editor.getValue()
+                    )
+                }}>
+                    Run
+                </button>
+            </div>
+        );
+    }
 }
 
 export default App;
