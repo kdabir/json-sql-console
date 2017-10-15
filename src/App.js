7cd033f69432db7549fa15855e3b4e0e40a30cd8
Kunal Dabir <kunal.dabir@gmail.com>
2017-10-15 12:19:53 +0530

    query working, show output on dev console

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import './App.css';
-
+import alasql from 'alasql';
 import AceEditor from "react-ace";
 import "brace/mode/json";
 import "brace/mode/sql";
@@ -10,8 +10,14 @@ import "brace/ext/language_tools";
 
 class App extends Component {
 
+    constructor(props){
+        super(props);
+        this.state =  {json:'[]', sql:'select * from ?'};
+    }
+
     runSql(json, sql) {
         console.log('running SQL', json, sql);
+        console.table(alasql(sql, [JSON.parse(json)]));
     }
 
     render() {
@@ -20,12 +26,13 @@ class App extends Component {
 
                 <div className="row">
                     <div className='column'>
-                        <p>Json Data</p>
+                        <p>Json Data (Array)</p>
                         <AceEditor
                             style={{width: '100%'}}
                             name="jsonEditor"
                             mode="json"
                             theme="solarized_dark"
+                            value={this.state.json}
                             ref='jsonEditor'/>
                     </div>
                     <div className='column'>
@@ -35,15 +42,16 @@ class App extends Component {
                             name='sqlEditor'
                             mode="sql"
                             theme="solarized_dark"
+                            value={this.state.sql}
                             ref='sqlEditor'/>
                     </div>
                 </div>
 
                 <button onClick={() => {
-                    this.runSql(
-                        this.refs.jsonEditor.editor.getValue(),
-                        this.refs.sqlEditor.editor.getValue()
-                    )
+                    let json = this.refs.jsonEditor.editor.getValue(),
+                        sql = this.refs.sqlEditor.editor.getValue();
+                    this.setState({json,sql})
+                    this.runSql(json,sql)
                 }}>
                     Run
                 </button>
